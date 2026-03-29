fb375da21cb8b520e566a1274307eeff30c814cf
Luiz Augusto Rodrigues <luizaugustorodrigues@gmail.com>
2026-03-29 00:18:39 -0300

    Ajuste de caminho

--- principal.sh
+++ principal.sh
@@ -11,8 +11,6 @@ export UPDATE
 
 # Diretorio do script principal
 TOOLS_DIR="${TOOLS_DIR:-$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)")}"
-# CORRETO
-#TOOLS_DIR="${TOOLS_DIR:-$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)}"
 
 # Diretorios dos modulos e configuracoes
 lib_dir="${lib_dir:-${TOOLS_DIR}/libs}"       # Diretorio dos modulos de biblioteca
